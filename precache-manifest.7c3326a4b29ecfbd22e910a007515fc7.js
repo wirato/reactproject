fc3db5aa7c1528d05e7cced15b3cf4808e61881c
Weerachai <weerachai.ch.61@ubu.ac.th>
2020-09-30 09:49:15 +0700

    Updates

---
+++ precache-manifest.7c3326a4b29ecfbd22e910a007515fc7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "375141afab3ec2346468bcf10ecbf210",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "13373f238aeaf6baa879",
+    "url": "/tic-tac-toe/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "44efbeb1a345ba240663",
+    "url": "/tic-tac-toe/static/js/2.6b633340.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tic-tac-toe/static/js/2.6b633340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13373f238aeaf6baa879",
+    "url": "/tic-tac-toe/static/js/main.a357c2a6.chunk.js"
+  },
+  {
+    "revision": "39f8774317d0b7d6102d",
+    "url": "/tic-tac-toe/static/js/runtime-main.3b27df4b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
